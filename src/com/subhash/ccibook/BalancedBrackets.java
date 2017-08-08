dc5eafef616f3588d7780e1619396ff6f8472e9e
Subhash Koganti <subhash.koganti@gmail.com>
2017-08-08 13:28:30 -0400

    Update BalancedBrackets.java

--- src/com/subhash/ccibook/BalancedBrackets.java
+++ src/com/subhash/ccibook/BalancedBrackets.java
@@ -7,12 +7,13 @@ import java.util.regex.*;
 public class Solution {
     
     public static boolean isBalanced(String expression) {
+        
         if(expression == null || expression.length()==0){
             return true;
         }
         
         char[] charArray = expression.toCharArray();
-        Stack stack = new Stack();
+        Stack stack = new Stack<Character>();
         for(int i=0 ;i < charArray.length ; i++){
             if( isOpeningBrace(charArray[i]) ){
                 stack.push(charArray[i]);
@@ -20,14 +21,41 @@ public class Solution {
                 if(stack.isEmpty()){
                     return false;
                 } else{
-                    
+                    Character temp = (char)stack.pop();
+                    if(temp != '('){
+                        return false;
+                    }
+                }
+            }else if(charArray[i] == '}'){
+                if(stack.isEmpty()){
+                    return false;
+                } else{
+                    Character temp = (char)stack.pop();
+
+                    if(temp != '{'){
+                        return false;
+                    }
+                }
+            }else if(charArray[i] == ']'){
+                if(stack.isEmpty()){
+                    return false;
+                } else{
+                    Character temp = (char)stack.pop();
+                    if(temp != '['){
+                        return false;
+                    }
                 }
             }
         }
         
+        if(!stack.isEmpty()){
+            return false;
+        }
+        
+        return true;
+        
     }
-    
-    private isOpeningBrace(char c ){
+    private  static boolean isOpeningBrace(char c ){
         if(c == '{' || c=='(' || c=='[' ){
             return true;
         }else{
